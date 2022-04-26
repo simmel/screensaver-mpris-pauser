04e15b74929835ebde659f59be8ba99114161e28
Simon Lundström <simlu+github@su.se>
2022-04-26 15:31:02 +0200

    Add consumer for GNOME Screensaver

--- screensaver_mpris_pauser.py
+++ screensaver_mpris_pauser.py
@@ -13,10 +13,22 @@ session_bus = dbus.SessionBus()
 
 def main():
     logging.info("Hello world!")
+    session_bus.add_signal_receiver(
+        pause_mpris,
+        dbus_interface="org.gnome.ScreenSaver",
+        path="/org/gnome/ScreenSaver",
+        interface_keyword="interface",
+        path_keyword="path",
+        member_keyword="member",
+    )
 
     # Create and run main loop
     loop = gi.repository.GLib.MainLoop()
     loop.run()
+
+
+def pause_mpris(enabled, **kwargs):
+    logging.debug("Screensaver %r=%r", kwargs["member"], enabled)
 
 
 if __name__ == "__main__":
