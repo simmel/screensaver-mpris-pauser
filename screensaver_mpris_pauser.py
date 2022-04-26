442c5cb9f0c91e607824cdfb181bdf266ec5527f
Simon Lundström <simlu+github@su.se>
2022-04-26 15:29:29 +0200

    Add dbus and glib loop

--- screensaver_mpris_pauser.py
+++ screensaver_mpris_pauser.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 import logging
 
+import dbus  # type: ignore
+import dbus.mainloop.glib  # type: ignore
+import gi.repository.GLib  # type: ignore
+
 logging.basicConfig(level=logging.INFO)
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+session_bus = dbus.SessionBus()
 
 
 def main():
     logging.info("Hello world!")
+
+    # Create and run main loop
+    loop = gi.repository.GLib.MainLoop()
+    loop.run()
 
 
 if __name__ == "__main__":
